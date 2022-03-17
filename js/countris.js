1d62bc07b7e6d2a22415efc0df431acc04f3616e
Rahim2725 <info.abdurrahim12.com>
2022-03-18 01:12:08 +0600

    distacchuar

--- js/countris.js
+++ js/countris.js
@@ -12,13 +12,35 @@ const displayCountries = countries =>{
     container.innerHTML = countriesHtml.join(' ');
 }
 
-const getCountriesHtml = country =>{
+const getCountriesHtml = ({name, flags, area}) =>{
     return `
         <div class="country">
-            <h2>${country.name.common}</h2>
-            <img src="${country.flags.png}">
+            <h2>${name.common}</h2>
+            <p>Area: ${area}</p>
+            <img src="${flags.png}">
         </div>
     `
 }
+
+/* option 01 
+ const getCountriesHtml = country =>{
+     const { flags, name} = conutry ;
+    return `
+        <div class="country">
+            <h2>${name.common}</h2>
+            <img src="${flags.png}">
+        </div>
+    `
+} */
+
+// original 
+// const getCountriesHtml = country =>{
+//     return `
+//         <div class="country">
+//             <h2>${country.name.common}</h2>
+//             <img src="${country.flags.png}">
+//         </div>
+//     `
+// }
 
 loadCountris();
